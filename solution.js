0da7274b583f9c81c345d6b29bcf93fb759438fe
SharukhGV <sharukhvelupillai@pursuit.org>
2023-07-11 18:49:49 -0400

    4 tests passing

--- solution.js
+++ solution.js
@@ -1,27 +1,48 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
+const isEveryNumGreaterThan2 = (nums) => {
   //
+// for (const num of nums){
+// if (num < 2) return false
+
+// }
+// return true
+const iGreaterThanThreshold = (currentValue) => currentValue >= 2;
+console.log(nums.every(iGreaterThanThreshold))
+return nums.every(iGreaterThanThreshold)
 };
 
-const isEveryWordShorterThan7 = () => {
+const isEveryWordShorterThan7 = (array) => {
   //
+  // if(word.length < 7){return true}
+  // return false
+
+  const WordShorterThan7 = (currentValue) => currentValue.length <7 ;
+return array.every(WordShorterThan7)
+
 };
 
 // Filter
 
-const arrayLessThan5 = () => {
+const arrayLessThan5 = (nums) => {
+
+return nums.filter(num => num < 5)
+
   //
 };
 
-const arrayOddLengthWords = () => {
+const arrayOddLengthWords = (words) => {
   //
+return words.filter(word => word.length%2===1)
+
 };
 
 // Find
 
 const firstValDivisibleBy4 = () => {
+
+  
   //
 };
 
