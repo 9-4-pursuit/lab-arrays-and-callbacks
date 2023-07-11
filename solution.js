b61365fc36df89a696fd21205b62f9c7111c4655
SharukhGV <sharukhvelupillai@pursuit.org>
2023-07-11 19:33:55 -0400

    more tests passing

--- solution.js
+++ solution.js
@@ -9,7 +9,10 @@ const isEveryNumGreaterThan2 = (nums) => {
 // }
 // return true
 const iGreaterThanThreshold = (currentValue) => currentValue >= 2;
-console.log(nums.every(iGreaterThanThreshold))
+
+//CHATGPT:  "iGreaterThanThreshold" is a function. It is a JavaScript arrow function defined using the const keyword. The function takes a single parameter called "currentValue" and returns a Boolean value indicating whether the currentValue is less than 40.
+
+// console.log(nums.every(iGreaterThanThreshold))
 return nums.every(iGreaterThanThreshold)
 };
 
@@ -40,34 +43,59 @@ return words.filter(word => word.length%2===1)
 
 // Find
 
-const firstValDivisibleBy4 = () => {
-
-  
+const firstValDivisibleBy4 = (array) => {
+// console.log()
+  return array.find(element=> !(element%4))
   //
 };
 
-const firstWordLongerThan4Char = () => {
+const firstWordLongerThan4Char = (array) => {
+
+return array.find(word => word.length>4)
   //
 };
 
 // Find Index
 
-const firstNumIndexDivisibleBy3 = () => {
+const firstNumIndexDivisibleBy3 = (array) => {
+
+  // const firstDiv3 = (currentValue) => !(currentValue%3) ;
+  const isDivby3 = (element) => element%3 === 0;
+
+  // let firstDivByThree =  array.find(num => num % 3 === 0)
+
+  // console.log(array.indexOf(array.find(num => (num%3===0))))
+  return array.findIndex(isDivby3)
+
+// return array.indexOf(firstDiv3)
+
   //
 };
 
-const firstWordIndexLessThan2Char = () => {
+const firstWordIndexLessThan2Char = (array) => {
   //
+  //
+  // findIndex
+  const lessthan2characters = (element) => element.length < 2;
+return array.findIndex(lessthan2characters)
 };
 
 // For Each
 
-const logValuesTimes3 = () => {
+const logValuesTimes3 = (array) => {
   //
+
+  array.forEach(element => console.log(element*3));
+
+
 };
 
-const logWordsWithExclamation = () => {
+const logWordsWithExclamation = (array) => {
   //
+  array.forEach(element => console.log(`${element}!`));
+
+
+
 };
 
 // Map
