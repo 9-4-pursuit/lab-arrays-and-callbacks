335119f5ef7fa457292f3395c9a3f5561ae59890
SamuelBAlba <brayanbalba24@gmail.com>
2023-07-18 22:49:06 -0400

    half

--- solution.js
+++ solution.js
@@ -1,41 +1,71 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
+const isEveryNumGreaterThan2 = (nums) => {
+    if (nums.every > 2) {
+      return true
+    }
+    return false
+
+    // return nums.every((nums) => nums >= 2);
+
+
+
+
   //
+
+
 };
 
-const isEveryWordShorterThan7 = () => {
+const isEveryWordShorterThan7 = (words) => {
+
+  if (words.every(word => word.length < 7)) {
+    return true
+  }
+  return false
   //
 };
 
 // Filter
 
-const arrayLessThan5 = () => {
+const arrayLessThan5 = (nums) => {
+
+  return nums.filter(num => num < 5)
   //
 };
 
-const arrayOddLengthWords = () => {
+const arrayOddLengthWords = (words) => {
+
+  return words.filter(word => word.length % 2 === 1)
   //
+  
 };
 
 // Find
 
-const firstValDivisibleBy4 = () => {
+const firstValDivisibleBy4 = (nums) => {
   //
+
+  return nums.find(num => num % 4 === 0)
+
+
 };
 
-const firstWordLongerThan4Char = () => {
+const firstWordLongerThan4Char = (words) => {
+
+  return words.find(word => word.length > 4)
   //
 };
 
 // Find Index
 
-const firstNumIndexDivisibleBy3 = () => {
+const firstNumIndexDivisibleBy3 = (nums) => {
+
+  return nums.findIndex(num => num % 3 === 0)
   //
 };
 
-const firstWordIndexLessThan2Char = () => {
+const firstWordIndexLessThan2Char = (word) => {
   //
 };
 
